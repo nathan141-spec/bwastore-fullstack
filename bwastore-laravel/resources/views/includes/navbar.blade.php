45e0793039ace74fdf3c45683e367660fb9f2174
Nathan Setiawan <nathanmabes@gmail.com>
2021-12-03 19:30:46 +0700

    Integrasi Login
    Integrasi Halaman - Update Navigation Section

--- bwastore-laravel/resources/views/includes/navbar.blade.php
+++ bwastore-laravel/resources/views/includes/navbar.blade.php
@@ -29,17 +29,68 @@
             <li class="nav-item">
               <a class="nav-link" href="#">Rewards</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/register.html">Sign Up</a>
+            @guest
+                <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">Sign Up</a>
             </li>
             <li class="nav-item">
               <a
                 class="btn btn-success nav-link px-4 text-white"
-                href="/login.html"
+                href="{{ route('login') }}"
                 >Sign In</a
               >
             </li>
+            @endguest
           </ul>
+
+          @auth
+              <!-- Desktop Menu -->
+          <ul class="navbar-nav d-none d-lg-flex">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <img
+                  src="/images/icon-user.png"
+                  alt=""
+                  class="rounded-circle mr-2 profile-picture"
+                />
+                Hi, {{ Auth::user()->name }}
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a>
+                <a class="dropdown-item" href="{{ route('dashboard-settings-account') }}"
+                  >Settings</a
+                >
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">Logout</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-inline-block mt-2" href="#">
+                <img src="/images/icon-cart-empty.svg" alt="" />
+              </a>
+            </li>
+          </ul>
+
+          <!-- Mobile Menu -->
+          <ul class="navbar-nav d-block d-lg-none">
+            <li class="nav-item">
+              <a class="nav-link" href="#"> Hi, Miko </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-inline-block" href="#"> Cart </a>
+            </li>
+          </ul>
+          @endauth
         </div>
       </div>
     </nav>
