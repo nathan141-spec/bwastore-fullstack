51d478470c36c1f3bc3cfec28271d1d9bc86e284
Nathan Setiawan <nathanmabes@gmail.com>
2021-12-05 19:12:58 +0700

    Integrasi Register
    Integrasi Halaman - Cek User

--- bwastore-laravel/resources/views/includes/navbar.blade.php
+++ bwastore-laravel/resources/views/includes/navbar.blade.php
@@ -1,79 +1,77 @@
 <nav
-      class="
-        navbar navbar-expand-lg navbar-light navbar-store
-        fixed-top
-        navbar-fixed-top
-      "
-      data-aos="fade-down"
+    class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
+    data-aos="fade-down"
+>
+    <div class="container">
+    <a href="{{ route('home') }}" class="navbar-brand">
+        <img src="/images/logo.svg" alt="Logo" />
+    </a>
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarResponsive"
     >
-      <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}">
-          <img src="/images/logo.svg" alt="Logo" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarResponsive"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item ">
-              <a class="nav-link" href="{{ route('home') }}">Home</a>
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="{{ route('home') }}" class="nav-link">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('categories')}}">Categories</a>
+                <a href="{{ route('categories') }}" class="nav-link">Categories</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Rewards</a>
+                <a href="#" class="nav-link">Rewards</a>
             </li>
             @guest
                 <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">Sign Up</a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="btn btn-success nav-link px-4 text-white"
-                href="{{ route('login') }}"
-                >Sign In</a
-              >
-            </li>
+                    <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a
+                    href="{{  route('login') }}"
+                    class="btn btn-success nav-link px-4 text-white"
+                    >Sign In</a
+                    >
+                </li>
             @endguest
-          </ul>
+        </ul>
 
-          @auth
-              <!-- Desktop Menu -->
-          <ul class="navbar-nav d-none d-lg-flex">
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <img
-                  src="/images/icon-user.png"
-                  alt=""
-                  class="rounded-circle mr-2 profile-picture"
-                />
-                Hi, {{ Auth::user()->name }}
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a>
-                <a class="dropdown-item" href="{{ route('dashboard-settings-account') }}"
-                  >Settings</a
-                >
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Logout</a>
-              </div>
-            </li>
+        @auth
+            <!-- Desktop Menu -->
+            <ul class="navbar-nav d-none d-lg-flex">
+                <li class="nav-item dropdown">
+                    <a
+                        href="#"
+                        class="nav-link"
+                        id="navbarDropdown"
+                        role="button"
+                        data-toggle="dropdown"
+                    >
+                        <img
+                            src="/images/icon-user.png"
+                            alt=""
+                            class="rounded-circle mr-2 profile-picture"
+                        />
+                        Hi, {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
+                        <a href="{{ route('dashboard-settings-account') }}" class="dropdown-item">
+                            Settings
+                        </a>
+                        <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                               Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                </li>
             <li class="nav-item">
               <a class="nav-link d-inline-block mt-2" href="#">
                 <img src="/images/icon-cart-empty.svg" alt="" />
